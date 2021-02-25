749e320ada8697fde0ae69f271967ffb9fe65356
James Hunt <james@niftylogic.com>
2021-02-25 00:08:35 -0500

    go fmt ftw

--- examples/simple.go
+++ examples/simple.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-  "fmt"
-  "os"
+	"fmt"
+	"os"
 
-  "github.com/jhunt/go-cli"
+	"github.com/jhunt/go-cli"
 )
 
 type Options struct {
-  Help     bool   `cli:"-h, --help"`
-  Version  bool   `cli:"-v, --version"`
-  Insecure bool   `cli:"-k, --insecure"`
-  URL      string `cli:"-U, --url"`
+	Help     bool   `cli:"-h, --help"`
+	Version  bool   `cli:"-v, --version"`
+	Insecure bool   `cli:"-k, --insecure"`
+	URL      string `cli:"-U, --url"`
 
-  Gen struct {
-    Length int     `cli:"-l, --length"`
-    Policy string  `cli:"-p, --policy"`
-  } `cli:"gen"`
+	Gen struct {
+		Length int    `cli:"-l, --length"`
+		Policy string `cli:"-p, --policy"`
+	} `cli:"gen"`
 }
 
 func main() {
-  var options Options
-  options.Gen.Length = 48 // a default
+	var options Options
+	options.Gen.Length = 48 // a default
 
-  command, args, err := cli.Parse(&options)
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "!!! %s\n", err)
-    os.Exit(1)
-  }
+	command, args, err := cli.Parse(&options)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "!!! %s\n", err)
+		os.Exit(1)
+	}
 
-  if command == "gen" {
-    path := ""
-    if len(args) > 0 {
-      path = args[0]
-    }
-    fmt.Printf("generating a password %d characters long", options.Gen.Length)
-    fmt.Printf("in the vault at %s\n", path)
-    // ...
-  }
+	if command == "gen" {
+		path := ""
+		if len(args) > 0 {
+			path = args[0]
+		}
+		fmt.Printf("generating a password %d characters long", options.Gen.Length)
+		fmt.Printf("in the vault at %s\n", path)
+		// ...
+	}
 }
